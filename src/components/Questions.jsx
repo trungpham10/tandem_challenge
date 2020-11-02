32d8e5125eeb54a07f68fa89028af58b193f11b5
Trung Pham <phamanhtrung@gmail.com>
2020-11-02 02:04:45 -0600

    fix 3 options question

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -12,7 +12,10 @@ export default class Questions extends Component {
                     <Answer answer={this.props.shuffledAnswers[0]} correctAnswer={this.props.data.correct} handleScore={this.props.handleScore} handleIndex={this.props.handleIndex} handleShuffle={this.props.handleShuffle}/>
                     <Answer answer={this.props.shuffledAnswers[1]} correctAnswer={this.props.data.correct} handleScore={this.props.handleScore} handleIndex={this.props.handleIndex} handleShuffle={this.props.handleShuffle}/>
                     <Answer answer={this.props.shuffledAnswers[2]} correctAnswer={this.props.data.correct} handleScore={this.props.handleScore} handleIndex={this.props.handleIndex} handleShuffle={this.props.handleShuffle}/>
-                    <Answer answer={this.props.shuffledAnswers[3]} correctAnswer={this.props.data.correct} handleScore={this.props.handleScore} handleIndex={this.props.handleIndex} handleShuffle={this.props.handleShuffle}/>
+                    {this.props.shuffledAnswers[3] ? (
+                        <Answer answer={this.props.shuffledAnswers[3]} correctAnswer={this.props.data.correct} handleScore={this.props.handleScore} handleIndex={this.props.handleIndex} handleShuffle={this.props.handleShuffle}/>
+                        ) : (<div></div>)
+                    }
                 </div>
             </div>
         )
